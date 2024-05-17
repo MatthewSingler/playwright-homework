0e94dfd059c44689ccdac45150990b67716c9084
MatthewSingler <matthew.singler@gmail.com>
2024-05-17 10:36:10 -0500

    Still trying locator for step 6

--- tests/inputFields.spec.ts
+++ tests/inputFields.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import exp from 'constants';
 
 test.beforeEach(async({page}) => {
     await page.goto('https://petclinic.bondaracademy.com/welcome')
@@ -16,28 +17,28 @@ test.describe('Pet types', () => {
         await page.getByRole('button', {name: "Edit"}).first().click()  //3. click on edit button for cat
         await expect(page.getByRole('heading', {name: "Edit Pet Type"})).toBeVisible() //4. assertion of Edit Pet Type header
 
-        const petTypeName = page.locator('#name')
-        await petTypeName.click()
-        await petTypeName.clear()
-        await petTypeName.fill('rabbit') //5. change pet name to rabbit and click update
-        const updatedPetTypeNameValue = await petTypeName.inputValue()
+        const editPetTypeName = page.locator('#name')
+        await editPetTypeName.click()
+        await editPetTypeName.clear()
+        await editPetTypeName.fill('rabbit') //5. change pet name to rabbit and click update
+        const updatedPetTypeNameValue = await editPetTypeName.inputValue()
         expect(updatedPetTypeNameValue).toEqual('rabbit')
         const updateButton = page.getByRole('button', {name: "Update"})
         await updateButton.click() //5. click update
 
         //await expect(petTypesTableName).toBeVisible()
-        //await expect(page.getByRole('row', {}))
-
-        //expect(updatedPetTypeNameValue).toContain //6. assertion for first pet in list is rabbit
+        page.locator('table tbody tr')
+        const petTypesListName = page.locator('table tbody tr [0]')
+        await expect(petTypesListName).toHaveValue('rabbit') //6. assertion for first pet in list is rabbit
 
         await page.getByRole('button', {name: "Edit"}).first().click() //7. click edit button for rabbit
         await expect(page.getByRole('heading', {name: "Edit Pet Type"})).toBeVisible()
-        await petTypeName.click()
-        await petTypeName.clear()
-        await petTypeName.fill('cat') //8. change name to cat and update
-        //await expect(updatedPetTypeNameValue).
+        await editPetTypeName.click()
+        await editPetTypeName.clear()
+        await editPetTypeName.fill('cat') //8. change name to cat and update
+        expect(updatedPetTypeNameValue).toEqual('cat')
         await updateButton.click()
-        //await expect(petTypeName.locator('td input').nth(0)).toHaveText('cat') //9. assertion that first pet in list is cat
+        await expect(petTypesListName).toHaveValue('cat')//9. assertion that first pet in list is cat
         
     })
 })
