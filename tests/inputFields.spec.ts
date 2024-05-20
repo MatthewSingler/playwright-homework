dcd0d7b7a10cb046c02b31f61b0377d01da7c4ac
MatthewSingler <matthew.singler@gmail.com>
2024-05-20 10:03:07 -0500

    Test #2 and #3 added

--- tests/inputFields.spec.ts
+++ tests/inputFields.spec.ts
@@ -38,4 +38,43 @@ test.describe('Pet types', () => {
         await expect(petTypesListName).toHaveValue('cat') //9. assertion that first pet in list is cat
         
     })
+
+    test('Cancel pet type update', async ({page}) => {
+        const petTypesTableName = page.getByRole('heading', {name: "Pet Types"})
+        await expect(petTypesTableName).toHaveText('Pet Types')  //2. assertion of Pet Types text displayed
+
+        await page.getByRole('button', {name: "Edit"}).nth(1).click() //3. click on edit button for dog
+        const editPetTypeName = page.locator('#name') //4. type new name moose
+        await editPetTypeName.click()
+        await editPetTypeName.clear()
+        await editPetTypeName.fill('moose')
+
+        const updatedPetTypeNameValue = await editPetTypeName.inputValue()
+        expect(updatedPetTypeNameValue).toEqual('moose') //5. Assertion for moose displayed on edit page
+
+        await page.getByRole('button', {name: "Cancel"}).click() //6. click cancel button
+
+        const petTypesListName = page.locator('tr input').nth(1)
+        await expect(petTypesListName).toHaveValue('dog') //7. assertion that dog is still displayed in the list of pet types
+    })
+
+    test('Pet type name validation', async ({page}) => {
+        const petTypesTableName = page.getByRole('heading', {name: "Pet Types"})
+        await expect(petTypesTableName).toHaveText('Pet Types')  //2. assertion of Pet Types text displayed
+
+        await page.getByRole('button', {name: "Edit"}).nth(2).click() //3. click on edit button for lizard
+        const editPetTypeName = page.locator('#name') //4. clear input field
+        await editPetTypeName.click()
+        await editPetTypeName.clear()
+
+        const nameIsRequiredMessage = page.locator('div .help-block').filter({hasText: "Name is required"}) //5. Assertion for 'name is required' message
+        expect(nameIsRequiredMessage).toBeVisible()
+
+        const updateButton = page.getByRole('button', {name: "Update"}) //6. click update button
+        await updateButton.click()
+
+        await expect(page.getByRole('heading', {name: "Edit Pet Type"})).toBeVisible() //7. assertion of Edit Pet Type page displayed
+        await page.getByRole('button', {name: "Cancel"}).click() //8. click cancel button
+        await expect(petTypesTableName).toHaveText('Pet Types')  //9. assertion of Pet Types text displayed
+    })
 })
